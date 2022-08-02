cdca644bf9cd3e03eede09e0663995e0e4149dbe
Zaynokov <vlerii99@mail.ru>
2022-08-02 23:21:35 +0300

    Lesson 1. Для заполнения БД нужно установить mimesis

--- todo/userapp/views.py
+++ todo/userapp/views.py
@@ -1,8 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 
 from .models import UserModel
+from .serializers import UserModelSerializer
 
 
 class UserModelViewSet(ModelViewSet):
-    serializer_class = UserModel
+    serializer_class = UserModelSerializer
     queryset = UserModel.objects.all()
