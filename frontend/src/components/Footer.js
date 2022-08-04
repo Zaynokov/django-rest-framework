eeede5cac76da6123e1494bf7baa9cd3a950d0f5
Zaynokov <vlerii99@mail.ru>
2022-08-04 22:24:29 +0300

    Lesson 2

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div>
+            <hr></hr>
+            <div>Контакты</div>
+            <div>О нас</div>
+        </div>
+    )
+}
+
+export default Footer
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'userapp',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,3 +128,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "userapp.UserModel"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
